70b9421333bcde9830fbc82eda71ee8ec46f4d03
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-12-24 15:00:34 -0500

    V11

--- server-socket.io-app1/index.js
+++ server-socket.io-app1/index.js
@@ -3,5 +3,5 @@ const server = require("./src");
 const PORT = 3001;
 
 server.listen(PORT, () => {
-  console.log(`server running at http://localhost:${PORT}`);
+  console.log(`server running at ${PORT} PORT`);
 });
