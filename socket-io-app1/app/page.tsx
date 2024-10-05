af793d22016edf0041bb110fca5858c03f572a3c
WilsonCollantesAponte <wilson.aponte000@gmail.com>
2024-10-05 11:51:44 -0500

    v12 - v2 images

--- socket-io-app1/app/page.tsx
+++ socket-io-app1/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { io } from "socket.io-client";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 // const socket = io("http://localhost:3001/");
 const socket = io("https://global-chat-uxv7.onrender.com/");
@@ -26,7 +27,6 @@ export default function Home() {
   const [messages, setMessages] = useState([]);
   const [isLoadingImages, setIsLoadingImages] = useState<boolean>(false);
   const [currentImages, setCurrentImages] = useState<Array<string>>([]);
-  const [refresh, setRefresh] = useState<number>(0);
 
   function handleSubmit(event) {
     console.log(currentImages);
@@ -58,53 +58,63 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-zinc-700 flex flex-col h-screen py-1.5 px-2.5">
-      <div className="text-white overflow-auto">
+    <div className="bg-zinc-700 flex flex-col min-h-screen">
+      <div className="text-white overflow-auto grow bg-red-300">
         {messages.map((message, index) => (
           <div
-            className={` w-2/3 rounded-xl mt-1.5
-          ${message.from === "Me" ? " bg-blue-600 ml-auto" : "bg-black/70"}
-          `}
+            className={`w-2/3 rounded-xl mt-1.5 ${
+              message.from === "Me" ? "bg-blue-600 ml-auto" : "bg-black/70"
+            }`}
             key={index}
           >
             <span className="py-1 px-2 relative break-all flex flex-col">
-              <span className=" font-extrabold italic">{message.from}</span>
-              <span className=" ml-auto text-sm">{message.body}</span>
+              <span className="font-extrabold italic">{message.from}</span>
+              <span className="ml-auto text-sm">{message.body}</span>
             </span>
           </div>
         ))}
       </div>
 
-      <div className="flex-grow flex mt-3">
+      {/* Formulario y área de imágenes */}
+      <div className="fixedX bg-orange-400 w-fullX bottom-0X">
+        {/* Imágenes a enviar */}
         {isLoadingImages ? (
           <div className="flex items-center justify-center">
             {/* <Spinner size="lg" /> */}
             Cargando...
           </div>
-        ) : (
-          <div className="flex gap-3 overflow-auto mt-1.5 px-4">
+        ) : currentImages.length > 0 ? (
+          <div className="flex gap-3 overflow-auto m-1.5 h-[125px]">
             {currentImages.map((aImage, index) => (
-              <img
-                className="size-36 object-cover rounded-md my-1.5"
+              <Image
+                className="object-cover rounded-md my-1.5"
                 key={index}
                 src={aImage}
                 alt="..."
+                height={120}
+                width={120}
               />
             ))}
           </div>
-        )}
-        <form className=" w-full flex self-end" onSubmit={handleSubmit}>
+        ) : null}
+
+        {/* Formulario de envío */}
+        <form
+          className="flex items-center justify-center"
+          onSubmit={handleSubmit}
+        >
           <input
             value={message}
-            className="rounded-md w-full"
+            className="rounded-md w-full grow"
             type="text"
-            placeholder="To send here..."
+            placeholder="Escribe un mensaje..."
             onChange={(event) => setMessage(event.target.value)}
           />
           <input
             type="file"
             accept="image/*"
             multiple
+            // className="ml-2"
             onChange={(event) => {
               setIsLoadingImages(true);
 
@@ -112,29 +122,32 @@ export default function Home() {
 
               if (!files?.length) return setIsLoadingImages(false);
 
-              for (const iterator of Array.from(files)) {
-                const readImage = new FileReader();
-                readImage.readAsDataURL(iterator);
-                readImage.onload = (event) => {
-                  currentImages.push(String(event.target?.result));
-                  setCurrentImages(currentImages);
+              const updatedImages = [];
+              let loadedImages = 0;
+
+              for (const file of Array.from(files)) {
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = (event) => {
+                  updatedImages.push(String(event.target?.result));
+                  loadedImages++;
+                  if (loadedImages === files.length) {
+                    setCurrentImages(updatedImages);
+                    setIsLoadingImages(false);
+                  }
                 };
               }
-              setRefresh(refresh + 1);
-              setTimeout(() => {
-                setRefresh(refresh + 1.81);
-                setIsLoadingImages(false);
-              }, 891);
             }}
           />
-          <button className=" text-white px-1.5">Send</button>
-          <img className=" h-12" src="/MatiMati_crop.jpg" alt="" />
+          <button className="text-white px-3 py-2 bg-blue-600 rounded-md ml-2">
+            Enviar
+          </button>
         </form>
       </div>
 
-      <div className=" mt-2 flex flex-col justify-center items-center">
-        <div className=" text-4xl text-white italic my-3.5">
-          Wilson's global chat
+      <div className="mt-2 flex flex-col justify-center items-center">
+        <div className="text-4xl text-white italic my-3.5">
+          Wilson's Global Chat
         </div>
         {/* <img className=" w-1/3 rounded-full" src="/MatiMati_crop.jpg" alt="" /> */}
       </div>
